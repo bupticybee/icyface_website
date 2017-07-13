dfcb09c5e28b5c469b89c9cf28062cafa01ac253
bupticybee <icybee@bupt.edu.cn>
2017-07-13 23:08:52 +0800

    detect demo v1

--- statics/js/detect.js
+++ statics/js/detect.js
@@ -42,15 +42,63 @@ angular.module('detect_demo',[])
         "statics/img/faces/face_6.jpg",
         "statics/img/faces/face_7.jpg"
     ]
+    function update_canvas(retdata){
+        canvas = document.getElementById('coveringCanvas')
+        ctx = canvas.getContext('2d')
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        canvas_width = canvas.clientWidth
+        canvas_height = canvas.clientHeight
+
+        image_big = document.getElementById('image-holder-right')
+        ori_height = image_big.naturalHeight;
+        ori_width = image_big.naturalWidth;
+        act_height = image_big.height;
+        act_width = image_big.width;
+
+        begin_with_x = 0;
+        /**
+         * [-[]-]
+         */
+        if (canvas_width > act_width){ 
+            begin_with_x = (canvas_width - act_width) / 2
+        }
+        for(var cords in retdata){
+            cords = retdata[cords]
+            left_c = cords[0];
+            top_c = cords[1];
+            right_c = cords[2];
+            bottom_c = cords[3];
+
+            left_c = left_c * act_width / ori_width;
+            right_c = right_c * act_width / ori_width;
+            
+            top_c = top_c * act_height / ori_height;
+            bottom_c = bottom_c * act_height / ori_height;
+            left_c += begin_with_x;
+            right_c += begin_with_x;
+            
+            y = top_c * 150 / canvas_height;
+            x = left_c * 300 / canvas_width;
+
+            recty = (bottom_c - top_c) * 150 / canvas_height;
+            rectx = (right_c - left_c) * 300 / canvas_width;
+            ctx.strokeRect(x,y,rectx,recty);
+
+        }
+
+    }
     this.click = function(one){
         this.selected = one;
         this.getBase64(one).then(function(imgbase64){
             data = {'img':imgbase64};
             $http.post('/face/face_detect',JSON.stringify(data)).then(function (data){
-                console.log(data);
+                update_canvas(data);
+
             },function errfun(e){
                 console.log('error during api');
             })
+            
+
         })
 
     }
