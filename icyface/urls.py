b0ded271a35a2620c6bd8445e15f5350b2d103e6
bupticybee <icybee@bupt.edu.cn>
2017-07-13 17:53:46 +0800

    modify to try to receive message from api backend

--- icyface/urls.py
+++ icyface/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import web
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = patterns('',
@@ -9,5 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'',include('web.urls'))
-)
+    url(r'',include('web.urls')),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- statics/js/detect.js
+++ statics/js/detect.js
@@ -1,5 +1,37 @@
 angular.module('detect_demo',[])
-.controller('detect_controller',function detect_controller(){
+.controller('detect_controller',function detect_controller($scope,$http){
+    function toDataURL(src, callback, outputFormat) {
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function() {
+            var canvas = document.createElement('CANVAS');
+            var ctx = canvas.getContext('2d');
+            var dataURL;
+            canvas.height = this.naturalHeight;
+            canvas.width = this.naturalWidth;
+            ctx.drawImage(this, 0, 0);
+            dataURL = canvas.toDataURL(outputFormat);
+
+            callback(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+        };
+        img.src = src;
+        if (img.complete || img.complete === undefined) {
+            img.src = "R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+            img.src = src;
+        }
+    }
+    this.getBase64 = function(URL){
+        return new Promise(
+            function(resolve,reject) {
+                toDataURL(
+                    URL,
+                    function(dataUrl) {
+                        resolve(dataUrl);
+                    }
+                );
+            }
+        );
+    }
     this.selected = "statics/img/faces/face_1.jpg";
     this.imageurls = [
         "statics/img/faces/face_1.jpg",
@@ -12,5 +44,14 @@ angular.module('detect_demo',[])
     ]
     this.click = function(one){
         this.selected = one;
+        this.getBase64(one).then(function(imgbase64){
+            data = {'img':imgbase64};
+            $http.post('/face/face_detect',data).then(function (data){
+                console.log(data);
+            },function errfun(e){
+                console.log('error during api');
+            })
+        })
+
     }
 })
